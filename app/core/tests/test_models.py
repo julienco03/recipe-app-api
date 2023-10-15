70c9f484c0527169623c04702d7d1351bd11fbf1
julienco03 <julian160903@gmail.com>
2023-10-15 12:58:12 +0200

    Require email address

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -4,6 +4,7 @@ Tests for models.
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelsTest(TestCase):
     """Test models."""
 
@@ -31,3 +32,8 @@ class ModelsTest(TestCase):
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, "sample123")
             self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """Test that creating a user without an email raises a ValueError."""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user("", "test123")
