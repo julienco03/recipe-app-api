8cc7d346428bd3570dd44c913980b2c44c57bfb7
Julian Klimek <85078802+julienco03@users.noreply.github.com>
2023-10-27 09:45:47 +0200

    Update urls.py

--- app/user/urls.py
+++ app/user/urls.py
@@ -8,8 +8,4 @@ app_name = "user"
 
 urlpatterns = [
     path("create/", views.CreateUserView.as_view(), name="create")
-<<<<<<< HEAD
 ]
-=======
-]
->>>>>>> 53d0d9b2e70d1248cffcd085f12e77c153687fa3
